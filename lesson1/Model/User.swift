1256684bf307db306e893d966655e2174da57f93
Anton <freshbreathe@gmail.com>
2021-02-03 02:54:16 +0300

    lesson3.1

---
+++ lesson1/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  lesson1
+//
+//  Created by Антон Сивцов on 03.02.2021.
+//
+
+import Foundation
+
+struct User {
+    let name: String
+    let lastName: String
+    var age: Int
+    var bio: String
+}
